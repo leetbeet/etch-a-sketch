7e38d08104a3f993f08c30fef5d59c8e0eb0bda3
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 10:29:21 +0800

    Add button that changes grid size

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row"); 
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
         const box = document.createElement("div");
         box.classList.add("box");
         row.appendChild(box);
     }
 
     container.appendChild(row);
+    }
 }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => {
-  box.addEventListener("mouseover", () => {
-    box.classList.add("highlighted");
-  });
+createGrid(16);
+
+function applyBoxListeners() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("highlighted");
+        });
+    });
+}
+
+applyBoxListeners();
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const input = prompt("Enter number of squares per side for new grid (1-100)");
+    
+    if (input === null) {
+        // do nothing
+    } else {
+        const size = parseInt(input);
+
+        if (!Number.isInteger(size) || size > 100 || size < 1) {
+            alert("Invalid input")
+        } else {
+            container.innerHTML = "";
+            createGrid(size);                
+            applyBoxListeners();
+        }
+    }
 });
