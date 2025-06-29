8d132b50b82fe8ea42ee46249ed801453e767c3b
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 09:15:44 +0800

    Set up 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row"); 
+
+    for (let j = 0; j < 16; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        row.appendChild(box);
+    }
+
+    container.appendChild(row);
+}
