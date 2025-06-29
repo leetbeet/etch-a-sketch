1579b7ff0d3468df761f757b2c5284d50dc77d52
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 10:44:15 +0800

    Change hover effect to change to a random colour

--- script.js
+++ script.js
@@ -15,20 +15,25 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
-
 function applyBoxListeners() {
     const boxes = document.querySelectorAll(".box");
     boxes.forEach(box => {
         box.addEventListener("mouseover", () => {
-            box.classList.add("highlighted");
+            const red = Math.floor(Math.random() * 256);
+            const green = Math.floor(Math.random() * 256);
+            const blue = Math.floor(Math.random() * 256);
+            box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         });
     });
 }
 
+// initial grid
+createGrid(16);
 applyBoxListeners();
 
+
 const button = document.querySelector("button");
+// create new grid
 button.addEventListener("click", () => {
     const input = prompt("Enter number of squares per side for new grid (1-100)");
     
